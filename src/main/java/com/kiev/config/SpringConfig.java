85ba32df5b180159fc1cb290c8315da02d858e91
Denys Zazeka <denys.zazeka@gmail.com>
2018-12-16 19:01:21 +0200

    Created test bean. Corectare

--- src/main/java/com/kiev/config/SpringConfig.java
+++ src/main/java/com/kiev/config/SpringConfig.java
@@ -1,7 +1,13 @@
 package com.kiev.config;
 
+import com.kiev.service.TestBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
+    @Bean
+    public TestBean getTestBean(){
+        return new TestBean("hello!");
+    }
 }
--- src/main/java/com/kiev/service/TestBean.java
+++ src/main/java/com/kiev/service/TestBean.java
@@ -1,4 +1,13 @@
 package com.kiev.service;
 
 public class TestBean {
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
