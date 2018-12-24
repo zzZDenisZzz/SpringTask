2c1ef96da7fd940a407dfcf8aac3abaa4d3c4e9e
Denys Zazeka <denys.zazeka@gmail.com>
2018-12-24 21:29:22 +0200

    Developed CRUD methods in UserDaoImpl

--- src/main/java/com/kiev/dao/UserDao.java
+++ src/main/java/com/kiev/dao/UserDao.java
@@ -6,5 +6,13 @@ import java.util.List;
 
 public interface UserDao {
 
+    void save(User user);
+
+    User getById(int id);
+
     List<User> findAll();
+
+    void delete(int id);
+
+    void update(User user);
 }
--- src/main/java/com/kiev/dao/UserDaoImpl.java
+++ src/main/java/com/kiev/dao/UserDaoImpl.java
@@ -16,6 +16,26 @@ public class UserDaoImpl implements UserDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    public void save(User user) {
+        String sql = "INSERT INTO user (name, email, age) VALUES (?,?,?)";
+        jdbcTemplate.update(sql, user.getName(), user.getEmail(), user.getAge());
+    }
+
+    public User getById(int id) {
+        String sql = "SELECT * FROM user WHERE id = ?";
+        return jdbcTemplate.queryForObject(sql, new UserMapper(), id);
+    }
+
+    public void delete(int id) {
+        String sql = "DELETE FROM user WHERE id = ?";
+        jdbcTemplate.update(sql, id);
+    }
+
+    public void update(User user) {
+        String sql = "UPDATE user SET name = ?,email = ?,age = ? WHERE id = ?";
+        jdbcTemplate.update(sql, user.getName(), user.getEmail(), user.getAge(), user.getId());
+    }
+
     public List<User> findAll() {
         String sql = "SELECT * FROM user";
         return jdbcTemplate.query(sql, new UserMapper());
